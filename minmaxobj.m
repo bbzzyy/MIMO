9e5e4dbfad10af977b8144e7c26240e1e07261ee
Zhongyan <bizhongyan@gmail.com>
2018-06-17 12:23:14 +0200

    Update minmaxobj.m

--- minmaxobj.m
+++ minmaxobj.m
@@ -1,6 +1,5 @@
 function f = minmaxobj(y,sigma,Ptot,tau_d,c,alpha)
-%UNTITLED4 此处显示有关此函数的摘要
-%   此处显示详细说明
+%The objective function
 
 calphap2 = c.*alpha.^2;
 term3 = sqrt(tau_d^2+c^2.*alpha.^4.*Ptot^2*y^2-2*tau_d*y.*(c.*alpha.^2*Ptot+2*sigma^2));
